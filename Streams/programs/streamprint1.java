97d14eb5a78db3359e1bc3162b461a510a01f024
le pire programmeur du monde <ajaydinakar6@gmail.com>
2018-09-30 18:42:59 -0500

    Create streamprint1.java

---
+++ Streams/programs/streamprint1.java
@@ -0,0 +1,13 @@
+
+import java.util.stream.*;
+import java.util.*;
+import java.nio.file.*;
+import java.io.IOException;
+class Main {
+  public static void main(String[] args) throws IOException  {
+    IntStream
+      .range(1,10)
+      .skip(5)
+      .forEach(x -> System.out.println(x));
+  }
+}
