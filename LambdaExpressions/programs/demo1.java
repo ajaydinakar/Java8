56b920289fbdfb7c8f6801429cf780ef94ea5841
le pire programmeur du monde <ajaydinakar6@gmail.com>
2018-10-01 13:06:53 -0500

    Update demo1.java

--- LambdaExpressions/programs/demo1.java
+++ LambdaExpressions/programs/demo1.java
@@ -1,1 +1,19 @@
-fefef
+
+interface FunInterface
+{ 
+    // An abstract function 
+    void abstractFunction(int x); 
+   
+}
+class Main {
+  public static void main(String[] args) {
+ 
+        // lambda expression to implement above 
+        // functional interface. This interface 
+        // by default implements abstractFun() 
+        FunInterface obj = (int arg)->System.out.println(arg*arg); 
+  
+        // This calls above lambda expression 
+        obj.abstractFunction(5);
+  }
+}
